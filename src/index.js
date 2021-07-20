4002fef56c7ce19a6725cf4ce5bc9a411e885093
Marshall Henhawke <marshall@allcareit.com>
2021-07-20 15:09:07 -0400

    peepo

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import SimpleGallerySlider from "./SimpleGallerySlider.vue";
+
+export default {
+ install(Vue, options) {
+  // Let's register our component globally
+  // https://vuejs.org/v2/guide/components-registration.html
+  Vue.component("simple-gallery-slider", SimpleGallerySlider);
+ }
+};
